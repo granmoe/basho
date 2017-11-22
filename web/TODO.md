cd2bdcedd9386787900a39de4735dd0ac28dd8f3
TimothyJohnK <Tim@VectorLab.com>
2017-11-21 20:55:44 -0600

    added keyframes fade in to anchor.js

--- web/TODO.md
+++ web/TODO.md
@@ -1,6 +1,7 @@
 # TODO:
 
-* hover styles for Anchor component, maybe a fade in (just so I can try keyframes with styled-components)
+* hover styles for Anchor component, maybe a fade in (just so I can try
+  keyframes with styled-components)
 * Add poem
 * Fancy text style for poem
   * use first-letter and first-line pseudo elements
--- web/components/anchor.js
+++ web/components/anchor.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
+const FadeIn = keyframes`
+@-webkit-keyframes NAME-YOUR-ANIMATION {
+  0%   { opacity: 0; }
+  100% { opacity: 1; }
+}
+@-moz-keyframes NAME-YOUR-ANIMATION {
+  0%   { opacity: 0; }
+  100% { opacity: 1; }
+}
+@-o-keyframes NAME-YOUR-ANIMATION {
+  0%   { opacity: 0; }
+  100% { opacity: 1; }
+}
+@keyframes NAME-YOUR-ANIMATION {
+  0%   { opacity: 0; }
+  100% { opacity: 1; }
+}
+`
+
 const Anchor = styled.a`
   text-decoration: none;
 
@@ -11,6 +30,7 @@ const Anchor = styled.a`
     text-decoration: none;
     color: inherit;
   }
+  animation: ${fadeIn} 2s linear;
 `
 
 export default ({ children, href }) => <Anchor href={href}>{children}</Anchor>
