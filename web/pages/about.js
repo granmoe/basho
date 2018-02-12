ef2029c89aaaf4abfe46a904e93d187c4af6df37
TimothyJohnK <Tim@VectorLab.com>
2018-02-11 23:50:33 -0600

    updated about.js

--- web/pages/about.js
+++ web/pages/about.js
@@ -23,8 +23,7 @@ const AboutPage = ({ theme }) => (
         basho.ai is Matt Granmoe, Tim Kirchhof, and the swarming Bayesian hive
         mind of a million adaptive neuro fuzzy inference AI bots. basho.ai
         implements a supervised deep learning system that generates hokku
-        ("haiku") that follow the core principles of Ensō (円相) (Zen
-        aesthetics).
+        ("haiku") that follow the core principles of Ensō 円相 (Zen aesthetics).
       </About>
     </Layout>
   </ThemeProvider>
